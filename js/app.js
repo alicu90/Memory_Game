1b6195289a4d86b809ee71ea6546d27fe7890f36
Alex Pricope <alicu90@gmail.com>
2018-05-17 15:31:44 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -2,17 +2,16 @@
  * Create a list that holds all of your cards
  */
 
- let list = ['fa-diamon','fa-diamon','fa-paper-plane-o','fa-paper-plane-o','fa-anchor','fa-anchor','fa-bolt','fa-bolt','fa-cube','fa-cube','fa-leaf','fa-leaf','fa-bicycle','fa-bicycle','fa-bomb','fa-bomb'];
- let cardFa = document.querySelectorAll('.card .fa')
- let deck = document.getElementsByClassName('deck');
- let card = document.querySelectorAll('.card');
- console.log(card);
+ const list = ['fa-diamon','fa-diamon','fa-paper-plane-o','fa-paper-plane-o','fa-anchor','fa-anchor','fa-bolt','fa-bolt','fa-cube','fa-cube','fa-leaf','fa-leaf','fa-bicycle','fa-bicycle','fa-bomb','fa-bomb'];
+ const deck = document.querySelector('.deck');
+ const cards = document.querySelectorAll('.card');
 
- let cardArray = [...card];
 
  //any array for the cards that the user opens 
-let openCards = [];
+let openedCards = [];
+let previousOpenedCard = null;
 let matchedCards = [];
+console.log(list);
 
 /*
  * Display the cards on the page
@@ -20,6 +19,53 @@ let matchedCards = [];
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
+
+// call the function which create the deck
+createDeck();
+
+// function declaration that shuffles the icon cards and places them in the DOM in random order
+function createDeck() {
+    const fragment = document.createDocumentFragment();
+
+    let shuffledList = shuffle(list);
+
+    // create <li> and <i> DOM elements
+    for(icon of shuffledList) {
+        // create new <li> element
+        const newlistItem = document.createElement('li');
+        newlistItem.className = "card";
+        // create new <i> element
+        const cardIconItem = document.createElement('i');
+        cardIconItem.className = card;
+        // append <i> element to the <li> element
+        listItem.appendChild(cardIconItem);
+        // append <li> element to the Deck
+        deck.appendChild(listItem);
+    }
+}
+
+// add event listener to cards
+deck.addEventListener('click', function(evt) {
+    // check the target if it is <li> element
+    if (evt.target.nodeName === 'li') {
+        // check that the card is not already matched
+        if (!evt.target.classList.contains("match")) {
+            if (openedCards.length < 2) {
+                if(previousOpenedCard === null) {
+                    previousOpenedCard = evt.target;
+                    openCards(evt);
+                    check(evt);
+                }
+                // open cards if the previously one is not opened
+                if (previousOpenedCard != evt.target) {
+                    previousOpenedCard = null;
+                    openCards(evt);
+                    check(evt);
+                }
+            }
+        }
+    }
+});
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -36,67 +82,32 @@ function shuffle(array) {
     return array;
 }
 
-function displayCards() {
-    this.classList.add('open');
-    this.classList.add('show');
-    this.classList.add('stop');
-    openCards.push(this);
+function openCards(x) {
+    x.target.classList.add("open");
+    const clickedCardIconClass = x.target.childNodes[0].getAttribute("class");
+    openedCards.push(clickedCardIconClass);
 }
 
-function pushing() {
-    if (openCards.length == 2) {
-        if (openCards[0].innerHTML != openCards[1].innerHTML) {
-           unmatching();
-        } else {
-            matching();
-        }
+// check if cards match
+function check (x) {
+    if (openedCards.length == 2) {
+        let cardOpen = document.querySelectorAll('.open');
+        cardOpen[0].classList.add("match");
+        cardOpen[1].classList.add('match');
+        cardOpen[0].classList.remove("open");
+        cardOpen[1].classList.remove("open");
+        openedCards.splice(0, 2);
+    } else {
+        let cardOpen = document.querySelectorAll('.open');
+        cardOpen[0].classList.add("no_match");
+        cardOpen[1].classList.add("no_match");
+        setTimeout(function() {
+            cardOpen[0].classList.remove("open", "no_match");
+            cardOpen[1].classList.remove("open", "no_match");
+            openedCards.splice(0, 2);
+        }, 1000);
     }
 }
-
-function matching() {
-    openCards[0].classList.toggle('match');
-    //penCards[0].classList.add('open', 'show');
-    openCards[1].classList.toggle('match'); 
-//penCards[1].classList.add('open','show');
-    matchedCards.push(openCards);
-  openCards=[];
-}
-
-function unmatching() {
-    setTimeout(function (){ 
-    openCards[0].classList.remove('open', 'show');
-    openCards[1].classList.remove('open', 'show');
-    openCards[0].classList.remove('stop');
-    openCards[1].classList.remove('stop');
-   openCards=[];
-    },1200);
-
-}
-
-
-for (let j = 0; j < cardArray.length; j++) {
-    cardArray[j].addEventListener('click', displayCards);
-    cardArray[j].addEventListener('click',pushing);
-};
-console.log(openCards);
-
-//show errors to user
-const displayErrors = (err) => {
-    closeErrors();
-    const errorMessage = '';
-    const errorDiv = document.createElement('div');
-    errorDiv.className = 'error-div';
-    errorDiv.innerHTML = errorMessage;
-    scorePanel.parentNode.insertBefore(errorDiv, scorePanel.nextSibling);
-    const closeButton = document.querySelector('.close');
-    closeButton.addEventListener('click', closeErrors);
-  }
-  
-  // removes the error message
-  const closeErrors = () => {
-    const errorDiv = document.querySelector('.error-div');
-    errorDiv ? errorDiv.remove() : null;
-  }
 
 
 
